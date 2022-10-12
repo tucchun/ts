d9df9e5f59c7581065751e3eab58c0e1d3a94383
C.Chun Tu <tucchun@hotmail.com>
2022-10-12 10:54:34 +0800

    ci: add eslint and commitlint

---
+++ .eslintrc.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+//   extends: ['standard'],
+  extends: ['standard', 'eslint-config-standard-with-typescript', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+    project: './tsconfig.json'
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error'
+  }
+}
